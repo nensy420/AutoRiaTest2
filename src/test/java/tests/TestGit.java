6bc3842f6bdbd64ec332b5f70ae5022dbf017bc8
Olha_Varvarska <nensy>
2018-09-17 15:07:54 +0300

    try

--- src/test/java/tests/TestGit.java
+++ src/test/java/tests/TestGit.java
@@ -17,7 +17,7 @@ public class TestGit {
         int a = 1;
         int b = 3;
         int c = a+b;
-        System.out.println("the result is:"+c);
+        System.out.println("the result is fjhpjjkpoip:"+c);
     }
 
 }
